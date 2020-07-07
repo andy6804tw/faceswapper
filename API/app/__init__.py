05dc4daa45315bc00b0d904c9e798a3a034bdf4f
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-07-07 22:20:46 +0800

    remove upload

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -3,8 +3,8 @@ import config
 from flask import Flask,request,render_template
 from flask_cors import CORS
 from app.controllers.swap import swap 
-from app.controllers.upload import upload, upFile
-from flask_uploads import configure_uploads
+# from app.controllers.upload import upload, upFile
+# from flask_uploads import configure_uploads
 
 
 
@@ -12,7 +12,7 @@ app=Flask(__name__)
 CORS(app)
 # app.config['UPLOADED_PHOTOS_DEST'] = os.getcwd()  # 文件储存地址
 app.config.from_object(config)
-configure_uploads(app, upFile)
+# configure_uploads(app, upFile)
 
 app.register_blueprint(swap, url_prefix='/swap')
 # app.register_blueprint(upload, url_prefix='/upload')
