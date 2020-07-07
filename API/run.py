0153ca30c3c28f03e9dd3b9d56dd9c3bb9b02eef
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-07-07 20:11:34 +0800

    modify

--- API/run.py
+++ API/run.py
@@ -1,5 +1,10 @@
 from app import app
+import config
 
+@app.route('/')
+def index():
+  return 'server started on '+str(config.PORT)+' PORT '+str(config.ENV)
+  
 if __name__ == '__main__':
   print(app.url_map)
-  app.run(debug=True,port=5000)
+  app.run(port=config.PORT)
